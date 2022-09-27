bec029c705981b37ae67c4bc6b045e7aa1918634
ewssential <eikewilke1989@live.de>
2022-09-27 21:24:47 +0200

    add test for three neighbors

--- game-of-life/python/tests/test_thing_fixture.py
+++ game-of-life/python/tests/test_thing_fixture.py
@@ -1,10 +1,14 @@
 import pytest
+
+
 class Cell:
 
     def __init__(self):
         pass
 
     def is_alive(self, neighbours):
+        if neighbours > 2:
+            return True
         return False
 
 
@@ -15,6 +19,10 @@ def cell():
 
 def test_cell_with_fewer_than_two_neighbours_dies(cell):
     assert cell.is_alive(1) is False
+
+
+def test_cell_with_three_neighbours_will_be_alive(cell):
+    assert cell.is_alive(3) is True
 
 
 
